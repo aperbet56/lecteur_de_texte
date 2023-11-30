e5fd5e7da3a2a59924370f0fe15b38cacd3f9e45
Annabelle Perbet <annabelleperbet@yahoo.fr>
2023-11-30 18:38:13 +0100

    Ajout de commentaires

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const btn = document.querySelector(".btn");
 const text = document.querySelector("#text");
 
 // Ecoute de l'événement "click" sur le bouton "lire le texte"
-
 btn.addEventListener("click", () => {
+  // SpeechSynthesisUtterance de l'API Web Speech représente une requête vocale. Il contient le contenu que le service vocal doit lire et des informations sur la manière de le lire.
   let utterance = new SpeechSynthesisUtterance(text.value);
   //console.log(utterance);
   utterance.voice = window.speechSynthesis.getVoices()[0];
