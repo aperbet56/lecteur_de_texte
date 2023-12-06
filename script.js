79db8610d37311be0feb86f6dc296c622e983914
Annabelle Perbet <annabelleperbet@yahoo.fr>
2023-12-06 16:59:12 +0100

    Création de la variable counter et de la fonction updateCounter

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Récupération des différents éléments
 const btn = document.querySelector(".btn");
 const text = document.querySelector("#text");
+const counter = document.querySelector(".counter");
 
 // Ecoute de l'événement "click" sur le bouton "lire le texte"
 btn.addEventListener("click", () => {
@@ -12,3 +13,16 @@ btn.addEventListener("click", () => {
   console.log(speechSynthesis.getVoices());
   window.speechSynthesis.speak(utterance);
 });
+
+// Déclaration de la fonction updateCounter qui permettre le calcul des lettres, espaces, signes de ponctuation, chiffres et symbole et bloque l'écriture d'autres éléments
+const updateCounter = () => {
+  counter.textContent =
+    text.value.length + "/" + text.getAttribute("maxLength"); // Récupérer et afficher le longueur du texte saisi par l'internaute
+  // remainingCounter.textContent =
+  // textarea.getAttribute("maxLength") - textarea.value.length; // calcul du nombre restant de caractère à saisir
+};
+
+// Appel de la fonction updateCounter
+updateCounter();
+
+text.addEventListener("keyup", updateCounter);
